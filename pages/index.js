58bc7fc7b273cc846687260d59a614e320dc2538
nilskerwer <uougk@student.kit.edu>
2022-07-28 11:47:32 +0200

    test animation bug fix try 2

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>lessssssss fuckin go!</title>
+        <title>Welcome to my app!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
