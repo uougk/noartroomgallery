d42f47e3e971d9b20ab9c1dc8780eed691e1401c
nilskerwer <uougk@student.kit.edu>
2022-07-28 13:06:28 +0200

    added lint, changed title in bug fix attempt 3

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="leggo!" />
+        <Header title="Welcome to my app!" />
         <div className="perspective">
     <div className="slider">
         <div className="corridor">
